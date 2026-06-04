f5f19d40a362f3c906fef56be100393b0a8b9e39
DSPharm <eugenrusu.serviciu@gmail.com>
2026-06-04 16:32:13 +0300

    Forma mai corecta

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {
-        "request": request
-    })
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
